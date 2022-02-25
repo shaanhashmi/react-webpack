9e789d8d356647be740ef4c0c85c033571cdcd20
Shaan <shaanh33@gmail.com>
2022-02-26 01:14:19 +0530

    React Typescript and Webpack setup from scratch

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './styles.css'
+import IMAGE from './react.png'
+import IMAGE_SVG from './react.svg'
+import Counter from './component/Counter';
+
+const App = () => {
+	return (
+		<div>
+			<h1>React Typescript Template and Webpack Setup {process.env.NODE_ENV} - {process.env.name} </h1>
+			<img src={IMAGE} alt="React Logo" width="300" height="200" />
+			<img src={IMAGE_SVG} alt="React Logo" width="300" height="200" />
+			<Counter/>
+		</div>
+	)
+}
+
+export default App
---
+++ src/component/Counter.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <button onClick={() => setCount(count => count + 1)}>Count - {count}</button>
+    </div>
+  );
+};
+
+export default Counter;
